9078e3b3a649391280e079f2e99579c33643dc07
emab <emab@odoo.com>
2021-12-29 11:00:41 -0600

    [UPDATE] code clean up and comments.

--- pixelsecurity_sale/models/sale.py
+++ pixelsecurity_sale/models/sale.py
@@ -1,35 +1,29 @@
 # -*- coding: utf-8
-from typing import Sequence
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
+
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    compute_field = fields.Float("weeee", compute='sort_lines')
-    
+    sort_lines_compute = fields.Float("Sort Lines", compute='sort_lines')
+
+    #Sort Order Lines to put labor lines at the end.
     def sort_lines(self):
         lines = []
-        labor_lines = []
-        big = None
         biggestN = 0
-        sequence = 0
+        #get the labor lines and also get the biggest sequence number on the OLs.
         for line in self.order_line:
             if(line.sequence >= biggestN):
                 biggestN = line.sequence
-                big = line
             if(line.is_labor_line):
-                labor_lines.append(line)
-            else:
                 lines.append(line)
         biggestN += 1
-        for line in labor_lines:
-            line.sequence = biggestN
-            lines.append(line)
-            biggestN += 1
+        #Set the sequence of the labor lines to the biggest sequence # + 1
         for line in lines:
-            line.sequence = sequence
-            sequence += 1
-        self.compute_field = 0
+            line.sequence = biggestN
+            biggestN += 1
+        self.sort_lines_compute = 0
+
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
@@ -58,23 +52,18 @@ class SaleOrderLine(models.Model):
             if not line.product_template_id.include_labor:
                 continue
             labor_product = line.product_template_id.labor_product.product_variant_id
-
+            #Set the parent order and sibling order lines.
             if not order_lines:
                 if(vals['order_id']):
                     order = self.env['sale.order'].browse(vals['order_id'])
                     order_lines = order.order_line
+            #Get the labor line that belongs to the current product on the line
             labor_line = self.env['sale.order.line'].search([('order_id','=',vals['order_id']),('product_id','=',labor_product.id)])
+            #Set the hours that will be added or set to the labor line
             labor_hours = line.product_template_id.labor_hours * line.product_uom_qty
-            print(line.product_template_id.labor_hours, line.product_uom_qty)
-            # print(line.product_uom_qty, "www")
-        
-            labor = self.calculate_labor(line)
-
+            #If the labor line exist, add the hours, else, create the labor line with the hours.
             if(labor_line):
-                # labor_line.price_unit += labor
                 labor_line.product_uom_qty += labor_hours
-                # labor_line.sequence = line.sequence + 1
-                print(line.sequence, "ssss")
             else:
                 self.env['sale.order.line'].create({
                         'order_id': line.order_id.id,
@@ -83,13 +72,12 @@ class SaleOrderLine(models.Model):
                         'product_uom': labor_product.uom_id.id,
                         'product_template_id': line.product_template_id.labor_product.id,
                         'product_uom_qty': labor_hours,
-                        # 'product_uom_qty': 1,
-                        # 'price_unit': labor,
                         'sequence': line.sequence + 1,
                         'parent_line_id': line.id,
                         'is_labor_line': True,
                     })
             new_lines.append(line)
+        #If there is an order, sort the lines.
         if(order != None):
             order.sort_lines()
         return new_lines
@@ -98,33 +86,22 @@ class SaleOrderLine(models.Model):
         if self._check_line_unlink():
             raise UserError(_('You can not remove an order line once the sales order is confirmed.\nYou should rather set the quantity to 0.'))
         for line in self:
+            #If a labor line exists in the product of the line that will be deleted, substract the corresponding hours from the labor line.
             if(line.product_template_id.include_labor):
                 labor_product = line.product_template_id.labor_product.product_variant_id
-                labor = self.calculate_labor(line)
                 labor_line = self.env['sale.order.line'].search([('order_id','=',line.order_id.id),('product_id','=',labor_product.id)])
                 labor_hours = line.product_id.product_tmpl_id.labor_hours * line.product_uom_qty
                 if(labor_line):
-                    if(labor_line.product_uom_qty - labor_hours > 0):
+                    #If the substraction of the hours is bigger than 0, then substract them, else, remove the labor line.
+                    if(labor_line.product_uom_qty - labor_hours >= 0):
                         labor_line.product_uom_qty -= labor_hours
                     else:
-                        print(labor_line in self, "pop")
                         if(labor_line not in self):
                             labor_line.unlink()
-                    # if(labor_line.price_unit - labor > 0):
-                    #     labor_line.price_unit -= labor
-                    # else:
-                    #     labor_line.unlink()
         return super(SaleOrderLine, self).unlink()
 
-    def calculate_labor(self, line, delta = None):
-        res = 0
-        if(delta != None):
-            res = line.product_template_id.labor_hours * delta * line.product_template_id.labor_rate
-        else:
-            res = line.product_template_id.labor_hours * line.product_uom_qty * line.product_template_id.labor_rate
-        return res
-
     def write(self, values):
+        #If there's a change in the quantity of the OL, add/substract hours to the corresponding labor line.
         if('product_uom_qty' in values):
             if(self.product_id.product_tmpl_id.include_labor):
                 labor_product = self.product_id.product_tmpl_id.labor_product.product_variant_id
@@ -132,8 +109,5 @@ class SaleOrderLine(models.Model):
                 delta = values['product_uom_qty'] - self.product_uom_qty 
                 labor_hours = self.product_id.product_tmpl_id.labor_hours * delta
                 labor_line.product_uom_qty += labor_hours
-                # delta = values['product_uom_qty'] - self.product_uom_qty
-                # labor = self.calculate_labor(self, delta)
-                # labor_line.price_unit += labor
         res = super(SaleOrderLine, self).write(values)
         return res
